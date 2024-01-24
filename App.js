8d43335b9f61b6585db64d96eb7d2a4d36d1c531
giorgisilagadze <silagadze.gio123@gmail.com>
2024-01-24 12:52:28 +0400

    first

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+} from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView
+        style={{
+          display: "flex",
+          gap: 10,
+        }}
+      >
+        <View style={styles.container}>
+          <Text style={styles.text}>ახლა შენ უყურებ ჩემ პირველ აპლიკაციას</Text>
+          <Text style={styles.text}>ხოა გლეჯავოი?</Text>
+          <Image source={logoImg} style={{ width: 200, height: 200 }} />
+          <Image
+            source={{ uri: "https://picsum.photos/200" }}
+            style={{ width: 200, height: 200 }}
+          />
+          <ImageBackground source={logoImg} style={{ width: 200, height: 200 }}>
+            <Text style={styles.text}>this is some text</Text>
+          </ImageBackground>
+          <StatusBar style="auto" />
+          <Button
+            title="Press"
+            onPress={() => console.log("deechira")}
+            color="red"
+            disabled={false}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +46,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    paddingTop: 50,
+  },
+  text: {
+    color: "white",
   },
 });
